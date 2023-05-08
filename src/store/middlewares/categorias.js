d30e2718d386efd4a0f7f44ba95569524086aa56
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-05-08 20:01:40 -0300

    fiz um listener para carregar as categorias

---
+++ src/store/middlewares/categorias.js
@@ -0,0 +1,49 @@
+import { createStandaloneToast } from '@chakra-ui/toast';
+import { createListenerMiddleware } from '@reduxjs/toolkit';
+import categoriasService from 'services/categorias';
+import {
+  adicionarTodasAsCategorias,
+  carregarCategorias,
+} from 'store/reducers/categorias';
+
+export const listener = createListenerMiddleware();
+const { toast } = createStandaloneToast();
+
+listener.startListening({
+  actionCreator: carregarCategorias,
+  effect: async (action, { dispatch, fork, unsubscribe }) => {
+    toast({
+      title: 'Carregando',
+      description: 'Carregando categorias',
+      status: 'loading',
+      duration: 2000,
+      isClosable: true,
+    });
+    const tarefa = fork(async (api) => {
+      await api.delay(500);
+      return await categoriasService.buscar();
+    });
+
+    const resposta = await tarefa.result;
+
+    if (resposta.status === 'ok') {
+      dispatch(adicionarTodasAsCategorias(resposta.value));
+      toast({
+        title: 'Sucesso!',
+        description: 'Categorias carregadas com sucesso!',
+        status: 'success',
+        duration: 2000,
+        isClosable: true,
+      });
+      unsubscribe();
+    } else if (resposta.status === 'rejected') {
+      toast({
+        title: 'Erro',
+        description: 'Erro na busca de categorias',
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+  },
+});
